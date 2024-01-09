b1f0b9c18750fab55a8e89dfe398bb10dcf6ca91
Alexsandre Macaulay Machado da Silva <alex@MacBook-Air-de-Alexsandre.local>
2024-01-09 16:21:55 -0300

    First version of ChatApp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
+import Alert from "react-bootstrap/Alert";
 
 function App() {
+  // Definindo componentes
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const [showWarning, setShowWarning] = useState(false);
+
+  // Função para lidar com as mensagens
+  const handleSendMessage = (e) => {
+    // Evitando que a página seja recarregada
+    e.preventDefault();
+
+    // Evitando que mensagens vazias sejam enviadas
+    if (newMessage === "") {
+      setShowWarning(true);
+      return;
+    } else {
+      setShowWarning(false);
+    }
+
+    // Armazenando mensagem no banco de dados
+    setMessages([...messages, newMessage]);
+    // Limpando o campo de mensagem
+    setNewMessage("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card className="m-3 p-3">
+        {messages.map((message) => (
+          <Card
+            bg="primary"
+            text="white"
+            className="p-2 w-fit-content ms-auto chat-bubble rounded-4"
+            key={message}
+          >
+            {message}
+          </Card>
+        ))}
+      </Card>
+
+      <Card className="m-3 p-3 ">
+        {showWarning && (
+          <Alert className="w-100">
+            Digite sua mensagem e clique em enviar
+          </Alert>
+        )}
+        <Form onSubmit={handleSendMessage}>
+          <Form.Group className="mb-3">
+            <Form.Label htmlFor="inputMessage">Mensagem</Form.Label>
+            <Form.Control
+              as="textarea"
+              id="inputMessage"
+              type="text"
+              style={{ height: "100px" }}
+              value={newMessage}
+              placeholder="Digite sua mensagem..."
+              onChange={(e) => setNewMessage(e.target.value)}
+            />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Enviar
+          </Button>
+        </Form>
+      </Card>
+    </>
   );
 }
 
